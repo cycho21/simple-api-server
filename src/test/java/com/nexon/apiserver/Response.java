76c1f5b66e54a6ec64d7a813d5bc7c8dbb57b289
chan8 <chan8@nexon.co.kr>
2017-02-06 14:36:15 +0900

    user testcase

---
+++ src/test/java/com/nexon/apiserver/Response.java
@@ -0,0 +1,7 @@
+package com.nexon.apiserver;
+
+/**
+ * Created by chan8 on 2017-02-06.
+ */
+public class Response {
+}
---
+++ src/test/java/com/nexon/apiserver/test/RandomStringGenerator.java
@@ -0,0 +1,7 @@
+package com.nexon.apiserver.test;
+
+/**
+ * Created by chan8 on 2017-02-06.
+ */
+public class RandomStringGenerator {
+}
