9caeb0087703f0ef29d15c5d837b5df9510932c9
chan8 <chan8@nexon.co.kr>
2017-02-07 10:08:15 +0900

    rewrite

--- src/main/java/com/nexon/apiserver/Server.java
+++ src/main/java/com/nexon/apiserver/Server.java
@@ -1,6 +1,7 @@
 package com.nexon.apiserver;
 
 import com.nexon.apiserver.dao.Dao;
+import com.nexon.apiserver.handler.ChatroomHandler;
 import com.nexon.apiserver.handler.RootHandler;
 import com.nexon.apiserver.handler.UserHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -12,6 +13,8 @@ import java.net.InetSocketAddress;
  * Created by Administrator on 2017-02-04.
  */
 public class Server {
+    private static final String BASE_URL = "/api/v1";
+    
 
     public static void main(String[] args) {
         Server server = new Server();
@@ -25,7 +28,8 @@ public class Server {
 //            dao.createUsersTable();
             HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
             server.createContext("/testuri", new RootHandler());
-            server.createContext("/api/v1/users", new UserHandler(dao));
+            server.createContext(BASE_URL + "/users", new UserHandler(dao));
+            server.createContext(BASE_URL + "/chatrooms", new ChatroomHandler(dao));
             server.setExecutor(null); // creates a default executor
             server.start();
         } catch (IOException e) {
--- src/main/java/com/nexon/apiserver/handler/ChatroomHandler.java
+++ src/main/java/com/nexon/apiserver/handler/ChatroomHandler.java
@@ -1,7 +1,24 @@
 package com.nexon.apiserver.handler;
 
+import com.nexon.apiserver.dao.Dao;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
 /**
  * Created by chan8 on 2017-02-07.
  */
-public class ChatroomHandler {
+public class ChatroomHandler implements HttpHandler {
+
+    private Dao dao;
+
+    public ChatroomHandler(Dao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
 }
